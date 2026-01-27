9b5f2b31c1dc0a3c77e0d40dcd5228d33ddaeb04
qalqa-dev <andreybas04@gmail.com>
2026-01-27 21:38:39 +0300

    fix: some fixes for hero section styles

---
+++ src/app/components/catalog-card/catalog-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-card',
+  imports: [],
+  templateUrl: './catalog-card.html',
+  styleUrl: './catalog-card.scss',
+})
+export class CatalogCard {
+
+}
