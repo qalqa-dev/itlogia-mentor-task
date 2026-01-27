6b315c9cbde613c8b0408ec265553b605a9f8d87
qalqa-dev <andreybas04@gmail.com>
2026-01-27 23:47:53 +0300

    feat: added form validation and submition

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -8,5 +8,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class Button {
   @Input() class: string = '';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
 }
--- src/app/pages/main/sections/form/form.ts
+++ src/app/pages/main/sections/form/form.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Button } from '../../../../components/button/button';
 
 @Component({
   selector: 'app-form',
-  imports: [Button],
+  imports: [Button, ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
-export class Form {}
+export class Form {
+  orderForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.orderForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      address: ['', [Validators.required, Validators.minLength(5)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[\d\s\+\-\(\)]+$/)]],
+    });
+  }
+
+  onNameInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+
+    const sanitizedValue = value.replace(/\./g, '');
+
+    if (value !== sanitizedValue) {
+      input.value = sanitizedValue;
+      this.orderForm.get('name')?.setValue(sanitizedValue, { emitEvent: false });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.orderForm.valid) {
+      console.log('Форма отправлена:', this.orderForm.value);
+      this.orderForm.reset();
+    } else {
+      Object.keys(this.orderForm.controls).forEach((key) => {
+        this.orderForm.get(key)?.markAsTouched();
+      });
+    }
+  }
+}
